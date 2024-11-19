e48fd22fc5775294c17b536d811e8d4479e3c395
surinnnnnn <didthfls2@naver.com>
2024-11-19 20:37:55 +0900

    add: channel test code 작성

--- src/channel/channel.controller.spec.ts
+++ src/channel/channel.controller.spec.ts
@@ -1,5 +1,16 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ChannelController } from './channel.controller';
+import { ChannelDto } from './dto/channel.dto';
+import { ChannelEntity } from './entities/channel.entity';
+import { ChannelService } from './channel.service';
+import { UserEntity } from 'src/user/entity/user.entity';
+
+const mockChannelService = {
+  createChannel: jest.fn(),
+  getChannel: jest.fn(),
+  updateChannel: jest.fn(),
+  removeChannel: jest.fn(),
+};
 
 describe('ChannelController', () => {
   let controller: ChannelController;
@@ -7,12 +18,87 @@ describe('ChannelController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [ChannelController],
+      providers: [
+        {
+          provide: ChannelService,
+          useValue: mockChannelService,
+        },
+      ],
     }).compile();
 
     controller = module.get<ChannelController>(ChannelController);
   });
+  const user: UserEntity = {
+    id: 1,
+    email: 'test@test.com',
+    password: 'testtest',
+    name: 'test',
+    nickname: 'test',
+    phoneNumber: '010-4444-4444',
+    likes: null,
+  };
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  const channelDto: ChannelDto = {
+    name: 'test',
+    profileImage: 'image',
+  };
+
+  const mockChannel: ChannelEntity = {
+    id: 1,
+    name: 'test',
+    userId: user.id,
+    profileImage: 'image',
+    video: null,
+  };
+
+  const updatedChannel: ChannelEntity = {
+    ...mockChannel,
+    ...channelDto,
+  };
+
+  const mockResponse = {
+    message: 'Channel deleted successfully',
+    deletedChannel: mockChannel,
+  };
+
+  describe('createChannel', () => {
+    it('채널을 생성하고 반환한다.', async () => {
+      mockChannelService.createChannel.mockResolvedValueOnce(mockChannel);
+
+      const result = await controller.createChannel(ChannelDto, user);
+      console.log(result);
+      expect(mockChannelService.createChannel).toHaveBeenCalledWith(ChannelDto, user);
+      expect(result).toEqual(mockChannel);
+    });
+
+    describe('getChannel', () => {
+      it('채널 정보를 반환한다.', async () => {
+        mockChannelService.getChannel.mockResolvedValueOnce(mockChannel);
+
+        const result = await controller.getChannel(1);
+
+        expect(mockChannelService.getChannel).toHaveBeenCalledWith(1);
+        expect(result).toEqual(mockChannel);
+      });
+    });
+
+    describe('updateChannel', () => {
+      it('업데이트된 채널 정보를 반환한다. ', async () => {
+        mockChannelService.updateChannel.mockResolvedValue(updatedChannel);
+        const result = await controller.updateChannel(user, channelDto);
+        expect(mockChannelService.updateChannel).toHaveBeenCalledWith(user, channelDto);
+        expect(result).toEqual(updatedChannel);
+      });
+    });
+
+    describe('deleteChannel', () => {
+      it('삭제된 채널 정보를 반환한다. ', async () => {
+        mockChannelService.removeChannel.mockResolvedValue(mockChannel);
+        const result = await controller.removeChannel(user);
+
+        expect(mockChannelService.removeChannel).toHaveBeenCalledWith(user);
+        expect(result).toEqual(mockChannel);
+      });
+    });
   });
 });
