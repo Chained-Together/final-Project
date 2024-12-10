b8444afeb9edcf5e9ec52e7049381dd23e1c8db1
Jacob-moon <53264081+Jacob-moon@users.noreply.github.com>
2024-12-10 21:56:19 +0900

    [fix] 레솔루젼 레포지토리 , 인터페이스 수정 및 서비스 리팩토링
    Co-Authored-By: NterChoi <choi16289@naver.com>

--- src/interface/impl/resolution.repository.ts
+++ src/interface/impl/resolution.repository.ts
@@ -3,7 +3,7 @@ import { IResolutionRepository } from '../resolution-interface';
 import { ResolutionEntity } from 'src/resolution/entities/resolution.entity';
 import { VideoEntity } from 'src/video/entities/video.entity';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { Repository, UpdateResult } from 'typeorm';
 
 @Injectable()
 export class ResolutionRepository implements IResolutionRepository {
@@ -11,6 +11,23 @@ export class ResolutionRepository implements IResolutionRepository {
     @InjectRepository(ResolutionEntity)
     private readonly repository: Repository<ResolutionEntity>,
   ) {}
+  findResolutionByvideoId(videoId: number): Promise<ResolutionEntity | null> {
+    return this.repository.findOne({
+      where: { video: { id: videoId } },
+    });
+  }
+  updateResolution(
+    videoId: number,
+    highResolutionUrl: string,
+    lowResolutionUrl: string,
+  ): Promise<UpdateResult> {
+    return this.repository.update(
+      {
+        video: { id: videoId },
+      },
+      { high: highResolutionUrl, low: lowResolutionUrl },
+    );
+  }
   createResolution(high: string, low: string, video: VideoEntity): ResolutionEntity {
     return this.repository.create({
       high,
