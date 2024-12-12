164c1b932fd9511607a34026a2c89b75ba461571
surinnnnnn <didthfls2@naver.com>
2024-12-12 19:35:55 +0900

    fix: auth module 등 수정

--- src/notification/notification.module.ts
+++ src/notification/notification.module.ts
@@ -5,10 +5,22 @@ import { UserEntity } from 'src/user/entities/user.entity';
 import { NotificationEntity } from './entities/notification.entity';
 import { NotificationController } from './notification.controller';
 import { NotificationService } from './notification.service';
+import { IChannelRepository } from 'src/interface/channel-interface';
+import { channelRepository } from 'src/interface/impl/channel.repository';
 
 @Module({
   imports: [TypeOrmModule.forFeature([ChannelEntity, NotificationEntity, UserEntity])],
   controllers: [NotificationController],
-  providers: [NotificationService],
+  providers: [
+    NotificationService,
+    {
+      provide: 'IChannelRepository',
+      useClass: channelRepository,
+    },
+    {
+      provide: 'INotificationRepository',
+      useClass: NotificationEntity,
+    },
+  ],
 })
 export class NotificationModule {}
